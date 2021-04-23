6b4bb1b38939dec5276c3d4cbf10c69f678b0fc7
Raoul Wols <raoulwols@gmail.com>
2021-04-23 11:13:12 +0200

    Docblocks

--- include/dnscpp/intrusivequeue.h
+++ include/dnscpp/intrusivequeue.h
@@ -1,10 +1,33 @@
+/**
+ *  intrusivequeue.h
+ *
+ *  Queue datastructure which also allows O(1)
+ *  removal from an item in the middle of the queue.
+ *
+ *  @author Raoul Wols <raoul.wols@copernica.com>
+ *  @copyright 2021 Copernica BV
+ */
+
+/**
+ *  Include guard
+ */
 #pragma once
 
+/**
+ *  Dependencies
+ */
 #include "intrusivequeueitem.h"
 #include <type_traits>
 
+/**
+ *  Begin namespace
+ */
 namespace DNS {
 
+/**
+ *  Class declaration
+ *  @tparam  T  The type of the items. The actual item type will be shared_ptr<T>.
+ */
 template <class T> class IntrusiveQueue final
 {
 public:
@@ -16,7 +39,10 @@ public:
      */
     void push(const std::shared_ptr<T> &item)
     {
+        // insert it at the back
         _q.push_back(item);
+
+        // update position
         item->position(std::prev(_q.end()));
     }
 
@@ -26,9 +52,16 @@ public:
      */
     std::shared_ptr<T> pop()
     {
-        auto result = _q.front();
+        // move it out from the front
+        auto result = move(_q.front());
+
+        // forget the position
         result->clearposition();
+
+        // pop front item
         _q.pop_front();
+
+        // done
         return result;
     }
 
@@ -39,14 +72,29 @@ public:
      */
     bool pop(IntrusiveQueueItem<T> &item)
     {
+        // if it doesn't have a position then we can't remove it
         if (!item.hasposition()) return false;
+
+        // get the remembered position from the item
         const auto pos = item.position();
+
+        // check if we're removing an item at the front of the queue
         const bool atfront = pos == _q.begin();
+
+        // remove it from the queue
         _q.erase(pos);
+
+        // forget the position
         item.clearposition();
+
+        // done
         return atfront;
     }
 
+    /**
+     *  Get a const reference to the item at the front of the queue.
+     *  @return const-ref
+     */
     typename std::list<std::shared_ptr<T>>::const_reference front() const noexcept { return _q.front(); }
 
     /**
@@ -62,6 +110,11 @@ public:
     bool empty() const noexcept { return _q.empty(); }
 
 private:
+    /**
+     *  Because we need stable iterators, we chose an std::list
+     *  as the underlying datastructure.
+     *  @var std::list<std::shared_ptr<T>>
+     */
     std::list<std::shared_ptr<T>> _q;
 };
 
--- include/dnscpp/intrusivequeueitem.h
+++ include/dnscpp/intrusivequeueitem.h
@@ -1,35 +1,94 @@
+/**
+ *  intrusivequeueitem.h
+ *
+ *  Mixin class to allow removal from an IntrusiveQueue from anywhere
+ *  in the queue.
+ *
+ *  @author Raoul Wols <raoul.wols@copernica.com>
+ *  @copyright 2021 Copernica BV
+ */
+
+/**
+ *  Include guard
+ */
 #pragma once
 
+/**
+ *  Dependencies
+ */
 #include <list>
 #include <memory>
 
+/**
+ *  Begin namespace
+ */
 namespace DNS {
 
+/**
+ *  Class declaration
+ *  @tparam  T  The derived type (your class)
+ */
 template <class T>
 class IntrusiveQueueItem
 {
 private:
+
+    /**
+     *  The position in the queue. Because we need stable iterators,
+     *  we chose a list as the underlying data structure for the queue.
+     *  @var iterator
+     */
     typename std::list<std::shared_ptr<T>>::const_iterator _position;
+
+    /**
+     *  Whether this item is inserted into a queue
+     *  @var bool
+     */
     bool _hasposition = false;
 
 protected:
+    /**
+     *  Constructors are protected and copying is prohibited.
+     */
     IntrusiveQueueItem() = default;
     virtual ~IntrusiveQueueItem() noexcept = default;
     IntrusiveQueueItem(const IntrusiveQueueItem&) = delete;
     IntrusiveQueueItem& operator=(const IntrusiveQueueItem&) = delete;
 
 public:
+
+    /**
+     *  Whether this item is sitting in a queue.
+     *  @return bool
+     */
     bool hasposition() const noexcept { return _hasposition; }
+
+    /**
+     *  Get the iterator
+     *  @return iterator
+     */
     typename std::list<std::shared_ptr<T>>::const_iterator position() const noexcept { return _position; }
+
+    /**
+     *  Set the position in the queue.
+     *  @param  value  iterator
+     */
     void position(const typename std::list<std::shared_ptr<T>>::const_iterator &value)
     {
         _position = value;
         _hasposition = true;
     }
+
+    /**
+     *  Clear the position in the queue.
+     */
     void clearposition()
     {
         _hasposition = false;
     }
 };
 
+/**
+ *  End namespace DNS
+ */
 }
